5a6426374dca60e5237973715a3b0b70f9d7df6f
fuhoang <fuhoang84@googlemail.com>
2017-03-05 12:12:36 +0000

    displaying offer your ride

--- resources/views/templates/dashboard.blade.php
+++ resources/views/templates/dashboard.blade.php
@@ -9,7 +9,34 @@
 
 			@foreach ($travels as $travel)
     			<p>This is travel {{ $travel->id }}</p>
+
+
+				<div class="panel panel-default">
+				  <div class="panel-heading">
+				    <h3 class="panel-title">{{ $travel->pickup }} -> {{ $travel->dropoff }}</h3>
+				  </div>
+				  <div class="panel-body">
+				    <span class="glyphicon glyphicon-calendar" aria-hidden="true"></span>
+	  				{{ Carbon\Carbon::parse($travel->travel_date)->format('D d M - H:i') }}
+				  </div>
+				</div>
+
+				@if($travel->round_trip)
+				<div class="panel panel-default">
+				  <div class="panel-heading">
+				    <h3 class="panel-title">{{ $travel->dropoff }} -> {{ $travel->pickup }}</h3>
+				  </div>
+				  <div class="panel-body">
+				    <span class="glyphicon glyphicon-calendar" aria-hidden="true"></span>
+	  				{{ Carbon\Carbon::parse($travel->return_date)->format('D d M - H:i') }}
+				  </div>
+				</div>
+				@endif
 			@endforeach
+
+
+
+
 		</div>
 
 
